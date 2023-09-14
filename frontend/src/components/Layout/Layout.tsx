fca755f37a9de3e5e1bcc211588a88bfd8d81441
giftedemon <ayanbeisenbek@gmail.com>
2023-09-14 22:26:47 +0600

    home page

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -4,23 +4,23 @@ import Header from "@components/Header/Header";
 import Footer from "@components/Footer/Footer";
 
 interface Props {
-	children: ReactNode;
-	className?: string;
+   children: ReactNode;
+   className?: string;
 }
 
 export default function Layout({ children, className }: Props) {
-	return (
-		<>
-			<Header />
-			<main
-				className={className}
-				style={{
-					flex: 1,
-				}}
-			>
-				{children}
-			</main>
-			<Footer />
-		</>
-	);
+   return (
+      <>
+         <Header />
+         <main
+            className={className}
+            style={{
+               flex: 1,
+            }}
+         >
+            {children}
+         </main>
+         <Footer />
+      </>
+   );
 }
