c54055f3667e45911c33b8b02393f1c3dfd1d43b
yungkhann <ertaev.19102007@gmail.com>
2023-09-16 20:34:39 +0600

    writing

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,17 +1,18 @@
-import { type ReactNode } from "react";
-import { type Metadata } from "next";
+import { type ReactNode } from 'react';
+import { type Metadata } from 'next';
 
-import "@assets/styles/default.scss";
+import '@assets/styles/default.scss';
+import Header from '@root/components/Header/Header';
 
 export const metadata: Metadata = {
-	title: "AIStudy",
-	description: "",
+  title: 'AIStudy',
+  description: '',
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-	return (
-		<html lang="en">
-			<body>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
